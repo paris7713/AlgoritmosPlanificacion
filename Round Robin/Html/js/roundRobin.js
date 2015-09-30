966a5bb63f7d866a1ceacd44ce739802208ff54c
paris7713 <paris_7713@hotmail.com>
2015-09-29 23:38:58 -0500

    Refinamiento de Round robin, solucion de inconvenientes

--- Round Robin/Html/js/roundRobin.js
+++ Round Robin/Html/js/roundRobin.js
@@ -5,36 +5,48 @@ var RoundRobin = function (){
 //---------------------------------------------------------------------------------------------------------------------------------
 RoundRobin.prototype.procesar = function (procesador){
 	if(procesador.colaListo.longitud > 0){
-		var raiz = procesador.colaListo.extraerNodo();	
-		procesador.colaCritico.insertarNodo(raiz.proceso, raiz.tiempo, raiz.metrica, raiz.recurso, procesador);
-		procesador.colaCritico.raiz.tiempo = procesador.colaCritico.raiz.tiempo - raiz.metrica;
-		setTimeout(function (){
-			if(procesador.colaCritico.raiz.tiempo > 0){		
-				//Analizar suspendido
-				raiz = procesador.colaCritico.raiz;
-				procesador.suspenderProceso(raiz.proceso, raiz.tiempo, raiz.metrica, raiz.recurso, procesador);
-				maquina.liberarRecurso(raiz.recurso);
-				procesador.colaCritico.extraerNodo();
-			}
-			else{
-				//Analizar terminado
-				raiz = procesador.colaCritico.raiz;
-				procesador.colaFinalizado.insertarNodo(raiz.proceso, 0, raiz.metrica, raiz.recurso, raiz.procesador);
-				maquina.liberarRecurso(raiz.recurso);
-				procesador.colaCritico.extraerNodo();
-			}		
-		}, raiz.metrica * 1000);
+		var raiz = procesador.colaListo.extraerNodo();
+		if(maquina.validarRecurso(raiz.recurso)){
+			raiz.estado = "critico";	
+			maquina.recursos[raiz.recurso].disponible = 0;
+			procesador.colaCritico.insertarNodo(raiz);
+			procesador.colaCritico.raiz.tiempo = procesador.colaCritico.raiz.tiempo - raiz.metrica;
+			setTimeout(function (){
+				if(procesador.colaCritico.raiz.tiempo > 0){		
+					//Analizar suspendido
+					raiz = procesador.colaCritico.raiz;
+					raiz.estado = "suspendido";
+					procesador.suspenderProceso(raiz);
+					maquina.liberarRecurso(raiz.recurso);
+					procesador.colaCritico.extraerNodo();
+				}
+				else{
+					//Analizar terminado
+					raiz.estado = "terminado";
+					raiz = procesador.colaCritico.raiz;
+					procesador.colaFinalizado.insertarNodo(raiz);
+					maquina.liberarRecurso(raiz.recurso);
+					procesador.colaCritico.extraerNodo();
+				}		
+			}, raiz.metrica * 1000);	
+		}
+		else{
+			raiz.estado = "bloqueado";
+			procesador.bloquearProceso(raiz);
+		}
 	}
 	
 	if(procesador.colaSuspendido.longitud > 0){
-		var raiz = procesador.colaSuspendido.extraerNodo();
 		setTimeout(function(){
-			procesador.colaListo.insertarNodo(raiz.proceso, raiz.tiempo, raiz.metrica, raiz.recurso, raiz.procesador);	
+			var raiz = procesador.colaSuspendido.extraerNodo();
+			raiz.estado = "listo";
+			procesador.colaListo.insertarNodo(raiz);	
 		}, 1000);
 	}
 	
 	if(procesador.colaBloqueo.longitud > 0){
 		var raiz = procesador.colaBloqueo.extraerNodo();
-		maquina.crearProceso(raiz.proceso, raiz.tiempo, raiz.metrica, raiz.recurso, raiz.procesador);
+		raiz.estado = "listo";
+		maquina.crearProceso(raiz);
 	}	
 }
