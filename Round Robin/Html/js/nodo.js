4a2803cee5b2c9f18650dc7d9ca63c6bfb448ef0
paris7713 <paris_7713@hotmail.com>
2015-10-19 23:14:08 -0500

    'Correccion' de la metrica

--- Round Robin/Html/js/nodo.js
+++ Round Robin/Html/js/nodo.js
@@ -240,9 +240,10 @@ Nodo.prototype.dibujarGanttNodo = function (){
 			
 			sumaTiempoPenalizacion.push(tiempoProporcionPenalizacion);	
 			for(var e = 0; e < sumaTiempoPenalizacion.length; e ++){
-				tiempoPenalizacionTotal = tiempoPenalizacionTotal + tiempoPenalizacionTotal[e];
+				tiempoPenalizacionTotal = tiempoPenalizacionTotal + sumaTiempoPenalizacion[e];
+				break;
 			}	
-			tiempoPenalizacionTotal = tiempoPenalizacionTotal/sumaTiempoEspera.length;
+			var tiempoPenalizacionTotalT = tiempoPenalizacionTotal/sumaTiempoPenalizacion.length;
 			
 			if(tiempoProporcionRespuestaTotal > 40){
 				$('#analisisP').empty();
@@ -257,7 +258,7 @@ Nodo.prototype.dibujarGanttNodo = function (){
 					flag = flag + 1;
 				}	
 				
-				if(tiempoPenalizacionTotal/10 <= 50){
+				if(tiempoPenalizacionTotalT <= 50){
 					flag = flag + 1;
 				}
 				
