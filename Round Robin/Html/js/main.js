2ea534d6813c75f22cc3705f54e234685fc09b11
paris7713 <paris_7713@hotmail.com>
2015-11-04 10:40:29 -0500

    Colas multiples SRTF

--- Round Robin/Html/js/main.js
+++ Round Robin/Html/js/main.js
@@ -1,6 +1,6 @@
 var maquina = new Maquina();
 
-maquina.agregarProcesador("Procesador1", fifo, jsf, fifo, "#maquina");
-maquina.agregarProcesador("Procesador2", fifo, jsf, fifo, "#maquina");
-maquina.agregarProcesador("Procesador3", fifo, jsf, fifo, "#maquina");
+maquina.agregarProcesador("Procesador1", roundRobin, jsf, fifo, "#maquina");
+maquina.agregarProcesador("Procesador2", roundRobin, jsf, fifo, "#maquina");
+maquina.agregarProcesador("Procesador3", roundRobin, jsf, fifo, "#maquina");
 var flag = 'Colas Multiples';
